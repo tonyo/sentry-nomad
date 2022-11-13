ef45df5baa24a72992059b0226bf4d243cf0ec2a
Anton Ovchinnikov <anton@tonyo.info>
2022-11-13 12:23:52 +0100

    Add sentry-go

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/tonyo/sentry-nomad
 
 go 1.19
+
+require (
+	github.com/getsentry/sentry-go v0.15.0 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- sentry-nomad.go
+++ sentry-nomad.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func testSDK() {
+	// Using SENTRY_DSN here
+	err := sentry.Init(sentry.ClientOptions{
+		// Enable printing of SDK debug messages.
+		// Useful when getting started or trying to figure something out.
+		Debug:            true,
+		TracesSampleRate: 0.0,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	// Set the timeout to the maximum duration the program can afford to wait.
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	testSDK()
+	fmt.Println("Done.")
 }
